66b3dfb9931e7533c47d0f5981b9a8dba540aca0
Rachel Wallis <rachelwalliswork@gmail.com>
2025-05-03 18:27:38 +0100

    Restructure DELETE handler with force-dynamic export

--- src/app/api/questions/[id]/DELETE.ts
+++ src/app/api/questions/[id]/DELETE.ts
@@ -1,6 +1,8 @@
 import { client } from '../../../../lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
